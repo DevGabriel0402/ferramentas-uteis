0e7743caec73e231590f806d65eef80d7f242b45
DevGabriel0402 <gabriel.profissional01@gmail.com>
2024-10-17 13:49:32 -0300

    Feat: Insert the Canva Pro Teste

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const products = [
       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJjW7pHbZAIeGR54U3K4OCy8B83Vg1204EUw&s",
     budge: "Vitalicio",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540515.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO62140510o7hMabg1e86304AC98`,
   },
   {
     id: 2,
@@ -21,6 +22,7 @@ const products = [
       "https://dcdn.mitiendanube.com/stores/002/385/428/products/2-db05415368daf7aa9d17146236794729-1024-1024.png",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540519.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO62140510HBOqpv4d2H63040792`,
   },
   {
     id: 3,
@@ -32,6 +34,7 @@ const products = [
       "https://dandelions.co.jp/blog/wp/wp-content/uploads/2024/06/image-3.png",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540529.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO621405106FWeCMIeiL63043781`,
   },
   {
     id: 4,
@@ -41,15 +44,17 @@ const products = [
     image: "./assets/combo+total.jpg",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540545.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO621405106VXfB4Dkhj6304AE01`,
   },
   {
     id: 5,
     name: "Combo Editor",
-    price: 34.9,
+    price: 32.9,
     description: "Aproveite tudo das melhores ferramentas desse mercado.",
     image: "./assets/combo+canva+capcut.jpg",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540532.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO621405109Cft5jNuwn6304AF45`,
   },
   {
     id: 6,
@@ -59,6 +64,7 @@ const products = [
     image: "./assets/combo+canva+chatgpt.jpg",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540532.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO621405109Cft5jNuwn6304AF45`,
   },
   {
     id: 7,
@@ -68,6 +74,7 @@ const products = [
     image: "./assets/combo+chatgpt+capcut.jpg",
     budge: "Anual",
     button: "Comprar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540532.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO621405109Cft5jNuwn6304AF45`,
   },
   {
     id: 8,
@@ -89,6 +96,7 @@ const products = [
       "https://static-media.hotmart.com/CQ4chJS9uQPfckKKE3YINruaEx8=/600x600/smart/filters:format(jpg):background_color(white)/hotmart/product_pictures/36c6c656-0214-4875-b609-ed5d19585a00/PROFILEFILTROS.png",
     budge: "Anual",
     button: "Acessar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540512.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO62140510U59Bxjnt7t6304F14D`,
   },
   {
     id: 10,
@@ -100,6 +108,19 @@ const products = [
       "https://hotmart.s3.amazonaws.com/product_pictures/016e3502-f683-429d-845f-ad1d702fbdfc/JPGLogodoCurso.jpg",
     budge: "Anual",
     button: "Acessar",
+    pix: `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e7520400005303986540512.905802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO62140510U59Bxjnt7t6304F14D`,
+  },
+  {
+    id: 11,
+    name: "Canva Pro Teste",
+    price: 0,
+    description:
+      "Clique em acessar para acessar uma turma de teste com tudo do canva pro liberado. Lembrando essa e uma turma de teste",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJjW7pHbZAIeGR54U3K4OCy8B83Vg1204EUw&s",
+    budge: "Teste",
+    button: "Acessar",
+    href: `https://www.canva.com/brand/join?token=X4CAsfBfLb-aIUbV1o_34Q&brandingVariant=edu&invitationDestinationType=group`,
   },
 ];
 
@@ -127,7 +148,8 @@ function handleButtonClick(productId) {
 
   // Verifica se o produto possui um link href
   if (product.href) {
-    window.location.href = product.href; // Abre o link
+    // window.location.href = product.href; //abre o link
+    window.open(product.href, "_blank"); //abre o link
   } else {
     openModal(productId); // Abre o modal se não houver href
   }
@@ -140,31 +162,34 @@ products.forEach((product) => {
 });
 
 // Funções para o modal
+// Funções para o modal
 const modal = document.getElementById("modal");
 const paymentModal = document.getElementById("payment-modal");
 const span = document.getElementsByClassName("close")[0];
 const closePaymentModal = document.getElementsByClassName(
   "close-payment-modal"
 )[0];
-let currentPixCode = "eusouonumerodopix"; // Código PIX fixo
+let currentPixCode = "";
+let currentProduct = null;
 
 function openModal(productId) {
-  const product = products.find((p) => p.id === productId);
-  document.getElementById("modal-product-name").textContent = product.name;
+  currentProduct = products.find((p) => p.id === productId);
+  document.getElementById("modal-product-name").textContent =
+    currentProduct.name;
   document.getElementById("modal-product-description").textContent =
-    product.description;
+    currentProduct.description;
   document.getElementById("modal-product-price").textContent =
-    product.price.toFixed(2);
-  document.getElementById("modal-product-image").src = product.image;
+    currentProduct.price.toFixed(2);
+  document.getElementById("modal-product-image").src = currentProduct.image;
+
+  // Armazenar o código PIX do produto atual
+  currentPixCode = currentProduct.pix;
 
   modal.style.display = "block";
 }
 
 // Evento para abrir o modal de pagamento
 document.getElementById("pay-button").addEventListener("click", function () {
-  // Atualiza o código PIX
-  currentPixCode = `00020126580014BR.GOV.BCB.PIX0136c3cbea24-7b44-4e26-b0dd-e406b19615e75204000053039865802BR5925Gabriel Lucas Aniceto Vie6009SAO PAULO62140510kiblh0KctW630413D6`;
-
   // Gerar QR Code
   const qrcodeContainer = document.getElementById("qrcode");
   qrcodeContainer.innerHTML = ""; // Limpar QR code anterior
